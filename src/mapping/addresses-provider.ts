202952eaf0baab1dd8f474e527d6484327aca07c
cannotflypig <document.sabakan@gmail.com>
2026-02-16 02:13:37 +0900

    fix: Correctly store Pool contract address for Health Factor calculation

--- src/mapping/addresses-provider.ts
+++ src/mapping/addresses-provider.ts
@@ -8,29 +8,34 @@ import {
 import { createMapContractToPool, getProtocol } from '../helpers/initializers';
 import { Pool } from '../../generated/schema';
 
-// Lightweight version: Only handle PoolConfigurator creation
+// Lightweight version: Handle Pool and PoolConfigurator creation
 export function handleProxyCreated(event: ProxyCreated): void {
   let newProxyAddress = event.params.proxyAddress;
   let contractId = event.params.id.toString();
+  let poolAddressesProviderAddress = event.address.toHexString();
 
-  if (contractId == 'POOL_CONFIGURATOR') {
-    PoolConfiguratorContract.create(newProxyAddress);
-
-    // Create or update Pool entity
-    let poolAddress = event.address.toHexString();
-    let pool = Pool.load(poolAddress);
+  // Create Pool entity when POOL is created
+  if (contractId == 'POOL') {
+    let pool = Pool.load(poolAddressesProviderAddress);
     if (pool == null) {
-      pool = new Pool(poolAddress);
+      pool = new Pool(poolAddressesProviderAddress);
       let protocol = getProtocol();
       pool.protocol = protocol.id;
       pool.active = true;
       pool.paused = false;
-      log.warning('Creating new pool entity: {}', [poolAddress]);
+      log.warning('Creating new pool entity: {}', [poolAddressesProviderAddress]);
     }
 
+    // Store the Pool contract address
     pool.pool = newProxyAddress;
     pool.save();
 
     createMapContractToPool(newProxyAddress, pool.id);
+  }
+
+  // Create PoolConfigurator template when POOL_CONFIGURATOR is created
+  if (contractId == 'POOL_CONFIGURATOR') {
+    PoolConfiguratorContract.create(newProxyAddress);
+    createMapContractToPool(newProxyAddress, poolAddressesProviderAddress);
   }
 }
